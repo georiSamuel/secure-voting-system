95b17d37ea180391592922d4e044cf52fce19463
Horlan Lacerda <horlansilva56@gmail.com>
2025-05-05 16:42:15 -0300

    Add files via upload

---
+++ SpringBoot/src/test/java/dev/java10x/ProjetoUrnaEletronica/ProjetoUrnaEletronicaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.ProjetoUrnaEletronica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUrnaEletronicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

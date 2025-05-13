ddd7fd226310651d6f329b898fe151ee21ea06b8
lethy-while <melo.lethycia@aluno.uepb.edu.br>
2025-05-13 18:47:16 -0300

    Corrigindo println para versão local

--- src/main/java/com/example/sistema_votacao/SistemaVotacaoApplication.java
+++ src/main/java/com/example/sistema_votacao/SistemaVotacaoApplication.java
@@ -10,8 +10,7 @@ import org.springframework.context.annotation.Bean;
 import com.example.sistema_votacao.modelo.Usuario;
 
 @SpringBootApplication
-public class SistemaVotacaoApplication {
-
+public class SistemaVotacaoApplication
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaVotacaoApplication.class, args);
 	}
@@ -23,15 +22,15 @@ CommandLineRunner init(UsuarioRepository usuarioRepo) {
     return args -> {
         if (usuarioRepo.findByCpf("123.456.789-00") == null) {
             Usuario admin = new Usuario();
-            admin.setCpf("123.456.789-00");
-            admin.setNome("Admin");
+            admin.setCpf("122.709.134-00");
+            admin.setNome("Administrador");
             admin.setSenha(BCrypt.hashpw("senha123", BCrypt.gensalt())); // criar classe ou sla, n sei usar bcrypt direito ainda
             admin.setDataCadastro(LocalDateTime.now());
             
             usuarioRepo.save(admin);
             System.out.println("Cadastro criado com sucesso");
         } else {
-            System.out.println("O cadastro já existe");
+            System.out.println("Cadastro já existente");
         }
     };
 }

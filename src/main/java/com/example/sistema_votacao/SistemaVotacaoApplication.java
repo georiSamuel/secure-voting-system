08f221b34ec8fbab928b353355f9f4c10c414b4b
lethy-while <melo.lethycia@aluno.uepb.edu.br>
2025-05-14 20:50:13 -0300

    Mensagem do commit

--- src/main/java/com/example/sistema_votacao/SistemaVotacaoApplication.java
+++ src/main/java/com/example/sistema_votacao/SistemaVotacaoApplication.java
@@ -7,30 +7,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.sistema_votacao.Repository.UsuarioRepository;
 import com.example.sistema_votacao.modelo.Usuario;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 @SpringBootApplication
-public class SistemaVotacaoApplication
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaVotacaoApplication.class, args);
-	}
+public class SistemaVotacaoApplication {
 
-}
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaVotacaoApplication.class, args);
+    }
 
-@Bean
-CommandLineRunner init(UsuarioRepository usuarioRepo) {
-    return args -> {
-        if (usuarioRepo.findByCpf("123.456.789-00") == null) {
-            Usuario admin = new Usuario();
-            admin.setCpf("122.709.134-00");
-            admin.setNome("Administrador");
-            admin.setSenha(BCrypt.hashpw("senha123", BCrypt.gensalt())); // criar classe ou sla, n sei usar bcrypt direito ainda
-            admin.setDataCadastro(LocalDateTime.now());
-            
-            usuarioRepo.save(admin);
-            System.out.println("Cadastro criado com sucesso");
-        } else {
-            System.out.println("Cadastro já existente");
-        }
-    };
+    @Bean
+    CommandLineRunner init(UsuarioRepository usuarioRepo) {
+        return args -> {
+            if (usuarioRepo.findByCpf("122.709.134-00") == null) {
+                Usuario admin = new Usuario();
+                admin.setCpf("122.709.134-00");
+                admin.setNome("Administrador");
+                admin.setSenha(BCrypt.hashpw("senha123", BCrypt.gensalt())); // ajeitar o bcrypt dps
+                admin.setDataCadastro(LocalDateTime.now());
+
+                usuarioRepo.save(admin);
+                System.out.println("Cadastro criado com sucesso");
+            } else {
+                System.out.println("Cadastro já existente");
+            }
+        };
+    }
 }

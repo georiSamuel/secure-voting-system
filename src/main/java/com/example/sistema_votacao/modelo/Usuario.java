2b5d92918d2cb2b3920c68a114e0390b21207e04
lethy-while <melo.lethycia@aluno.uepb.edu.br>
2025-05-13 23:22:42 -0300

    Atualizando as entidades Usuario, Voto e Votacao

--- src/main/java/com/example/sistema_votacao/modelo/Usuario.java
+++ src/main/java/com/example/sistema_votacao/modelo/Usuario.java
@@ -2,19 +2,38 @@ package com.example.sistema_votacao.modelo;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 public class Usuario {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(unique = true, nullable = false)
     private String cpf;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private String senha;
+
     private LocalDateTime dataCadastro;
-    
-    
+
+    private boolean jaVotou = false;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<Voto> votos;
+
+    @PrePersist
+    public void prePersist() {
+        this.dataCadastro = LocalDateTime.now();
+    }
+
+    // Getters e Setters
+
     public Long getId() {
         return id;
     }
@@ -53,5 +72,21 @@ public class Usuario {
 
     public void setDataCadastro(LocalDateTime dataCadastro) {
         this.dataCadastro = dataCadastro;
+    }
+
+    public boolean isJaVotou() {
+        return jaVotou;
+    }
+
+    public void setJaVotou(boolean jaVotou) {
+        this.jaVotou = jaVotou;
+    }
+
+    public List<Voto> getVotos() {
+        return votos;
+    }
+
+    public void setVotos(List<Voto> votos) {
+        this.votos = votos;
     }
 }
---
+++ src/main/java/com/example/sistema_votacao/modelo/Voto.java
@@ -0,0 +1,5 @@
+package com.example.sistema_votacao.modelo;
+
+public class Voto {
+
+}

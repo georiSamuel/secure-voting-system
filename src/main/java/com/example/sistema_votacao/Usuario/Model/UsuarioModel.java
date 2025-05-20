860f8a5613b45dd29aedc6d55e4f11b6c33bacc4
Horlanlacerda <horlansilva56@gmail.com>
2025-05-19 22:02:35 -0300

    Edição usuário

--- src/main/java/com/example/sistema_votacao/Usuario/Model/UsuarioModel.java
+++ src/main/java/com/example/sistema_votacao/Usuario/Model/UsuarioModel.java
@@ -1,6 +1,8 @@
-package com.example.sistema_votacao.Cadastro.Model;
+package com.example.sistema_votacao.Usuario.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,22 +13,20 @@ import lombok.NoArgsConstructor;
 
 
 @Entity
-@Table(name = "tb_cadastro")
+@Table(name = "tb_usuario")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class CadastroModel {
+public class UsuarioModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String nome;
-    private String cpf;
     private String email;
     private String senha;
 
-    
-
-
-    
+    @Enumerated(EnumType.STRING) // essa anotação diz ao banco de dados para salvar como um nome literal do enum, como ADMIN E COMUM;
+    private TipoUsuario.Tipo tipo;    
 }
